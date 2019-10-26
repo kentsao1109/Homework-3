974649dfcfef5a4921765f0413f552a28ac007f5
Ken Tsao <ntsao@marathonoil.com>
2019-10-26 18:27:15 -0500

    Modified the physicians show page

--- app/models/physician.rb
+++ app/models/physician.rb
@@ -1,5 +1,6 @@
 class Physician < ApplicationRecord
   has_many :offices, inverse_of: :physician
+  has_many :appointments
 
   accepts_nested_attributes_for :offices, reject_if: :all_blank, allow_destroy: true
 end
